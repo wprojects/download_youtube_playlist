ace1ac72e13e6ddd78744000ab788cd13bb94992
wprojects <57518634+wprojects@users.noreply.github.com>
2023-03-30 07:29:46 -0500

    Create get_music.py

---
+++ get_music.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import re
+import pytube
+import youtube_dl
+
+requests.packages.urllib3.disable_warnings()
+
+# Insert Playlist ID --> Click the playlist and copy the part after list= in the browser:
+# https://www.youtube.com/watch?v=uHeQBDQlPD8&list=<get_playlist_id_here> (Last string after list=)
+# https://www.youtube.com/watch?v=i3avL_uAvLg&list=PLZ0LWy2i9_iPRk2UgeceeB8eZ3g_yucaE
+
+#Path to download to
+folder_path = "/home/samwins/side_projects/music_type_shit/Songs"
+playlist_id = 'PLZ0LWy2i9_iPRk2UgeceeB8eZ3g_yucaE'
+
+
+
+r = requests.get(f'https://www.youtube.com/watch?v=uHeQBDQlPD8&list={playlist_id}')
+page = r.text
+soup=bs(page,'html.parser')
+res=soup.find_all('a',{'class':'pl-video-title-link'})
+pattern = r"/watch\?[\w=&%]+"
+matches = re.findall(pattern, page)
+og_array = []
+song_count = 0
+
+for match in matches:
+    og_music = f"https://youtube.com{match}"
+    if og_music in og_array:
+        continue
+    if og_music == "https://youtube.com/watch?v":
+        continue
+    else:
+        song_count += 1
+        og_array.append(og_music)
+        print(f"Full Link: {og_music} | Song Count: {song_count}")
+
+        try:
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+                'outtmpl': f"{folder_path}/%(title)s.%(ext)s"
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([og_music])
+        except Exception as e:
+            print(f"Error downloading {og_music}: {e}")
+            continue
+
+print('Total Songs: ', song_count)
